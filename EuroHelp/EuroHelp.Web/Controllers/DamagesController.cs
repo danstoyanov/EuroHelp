94413b642e4060a18078fc7dd43f1bea4443184a
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-03-01 17:47:26 +0200

    🆕 Add demage register view !

---
+++ EuroHelp/EuroHelp.Web/Controllers/DamagesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EuroHelp.Web.Controllers
+{
+    public class DamagesController : Controller 
+    {
+        public DamagesController()
+        {
+
+        }
+
+        public IActionResult DemageRegister()
+        {
+            return View();
+        }
+    }
+}
