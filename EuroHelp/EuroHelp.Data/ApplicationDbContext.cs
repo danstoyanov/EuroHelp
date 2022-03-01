27d9ac73a2a926db4a8717ef3237f7c106d0aea2
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-03-01 19:19:49 +0200

    🆕 Add in .Data project AppDbContext file !

---
+++ EuroHelp/EuroHelp.Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuroHelp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
