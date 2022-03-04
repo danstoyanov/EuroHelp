a09be86c6417f48c9891b50fd083b677e1d905e1
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-03-04 14:14:25 +0200

    🆕 Add Login View file ! - Add UsersController class ! - Make some fixes in the _Layout file !

---
+++ EuroHelp/EuroHelp.Web/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EuroHelp.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        public UsersController()
+        {
+
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
