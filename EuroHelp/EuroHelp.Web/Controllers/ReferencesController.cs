5b2c16afda9582bde0fa9f47c89b921f819823b2
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-03-04 13:52:54 +0200

    🆕 Add GenerateReference View file ! - Add GenerateReference controller class !

---
+++ EuroHelp/EuroHelp.Web/Controllers/ReferencesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EuroHelp.Web.Controllers
+{
+    public class ReferencesController : Controller
+    {
+        public ReferencesController()
+        {
+
+        }
+
+        public IActionResult GenerateReference()
+        {
+            return View();
+        }
+    }
+}
