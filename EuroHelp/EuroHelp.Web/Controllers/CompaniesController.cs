cf7bbc7f7b43b7943b0688707425046dd66cf735
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-03-04 13:32:09 +0200

    🆕 Add new CompanyMembers view page ! - Add new Companies Controller class !

---
+++ EuroHelp/EuroHelp.Web/Controllers/CompaniesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EuroHelp.Web.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        public CompaniesController()
+        {
+
+        }
+
+        public IActionResult CompanyMembers()
+        {
+            return View();
+        }
+    }
+}
