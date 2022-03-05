eaeb4a8c67c6946bfc42f5cade16ee7e8d462613
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-03-05 17:06:46 +0200

    ✔ Clear the code in Program.cs file !

--- EuroHelp/EuroHelp.Web/Program.cs
+++ EuroHelp/EuroHelp.Web/Program.cs
@@ -5,7 +5,6 @@ using EuroHelp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -17,7 +16,6 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -25,16 +23,14 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseHttpsRedirection()
+    .UseStaticFiles()
+    .UseRouting()
+    .UseAuthentication();
 
-app.UseRouting();
-
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
