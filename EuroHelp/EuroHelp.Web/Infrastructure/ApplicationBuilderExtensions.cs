e94af3942216ce61c90be857ff1e6bd4c28ca880
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-03-09 15:09:41 +0200

    ❌ Tried to fix the references between projects !

---
+++ EuroHelp/EuroHelp.Web/Infrastructure/ApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using EuroHelp.Data;
+using EuroHelp.Data.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace EuroHelp.Web.Infrastructure
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder PrepareDatabase 
+            (this IApplicationBuilder app)
+        {
+            using var scopedServices = app.ApplicationServices.CreateScope();
+            var data = scopedServices.ServiceProvider.GetService<EuroHelpDbContext>();
+
+            data.Database.Migrate();
+
+            return app;
+        }
+    }
+}
