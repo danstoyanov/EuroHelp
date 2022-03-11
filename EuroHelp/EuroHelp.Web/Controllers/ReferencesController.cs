dff3cca62f66404b47ebd8d85dd7afce30c15367
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-03-11 16:10:51 +0200

    🆕 Generate Damage reference ! ✔ Add thee post methods to generate excel file ! ✔ Change some functionalities in Reference controller 🛠 Fix the Reference View file !

--- EuroHelp/EuroHelp.Web/Controllers/ReferencesController.cs
+++ EuroHelp/EuroHelp.Web/Controllers/ReferencesController.cs
@@ -1,17 +1,90 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+using EuroHelp.Data;
+using EuroHelp.Web.Models.References;
 
 namespace EuroHelp.Web.Controllers
 {
     public class ReferencesController : Controller
     {
-        public ReferencesController()
-        {
+        private readonly EuroHelpDbContext data;
 
+        public ReferencesController(EuroHelpDbContext data)
+        {
+            this.data = data;
         }
+
 
         public IActionResult GenerateReference()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult GenerateByCompanyName(ExportFile file)
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine("Name, CompanyName");
+
+            var damages = this.data.Damages
+                .Where(d => d.CompanyName == "Хелпми ООД")
+                .ToList();
+
+            foreach (var damage in damages)
+            {
+                builder.AppendLine($"{damage.Name}, {damage.CompanyName}");
+            }
+            
+            var data = Encoding.UTF8.GetBytes(builder.ToString());
+            var result = Encoding.UTF8.GetPreamble().Concat(data).ToArray();
+
+            return File(result, "text/csv", "damagesByCompany.csv");
+        }
+
+        [HttpPost]
+        public IActionResult GenerateByPretentionNumber(ExportFile file)
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine("Name, Property");
+
+            var damages = this.data.Damages
+                .Where(d => d.Property == "Няма")
+                .ToList();
+
+            foreach (var damage in damages)
+            {
+                builder.AppendLine($"{damage.Name}, {damage.Property}");
+            }
+
+            var data = Encoding.UTF8.GetBytes(builder.ToString());
+            var result = Encoding.UTF8.GetPreamble().Concat(data).ToArray();
+
+            return File(result, "text/csv", "damagesPretention.csv");
+        }
+
+        [HttpPost]
+        public IActionResult GenerateByConfirmation(ExportFile file)
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine("Name, CompanyName");
+
+            var damages = this.data.Damages
+                .Where(d => d.CompanyName == "Хелпми ООД")
+                .ToList();
+
+            foreach (var damage in damages)
+            {
+                builder.AppendLine($"{damage.Name}, {damage.CompanyName}");
+            }
+
+            var data = Encoding.UTF8.GetBytes(builder.ToString());
+            var result = Encoding.UTF8.GetPreamble().Concat(data).ToArray();
+
+            return File(result, "text/csv", "damagesByConfirm.csv");
         }
     }
 }
---
+++ EuroHelp/EuroHelp.Web/Models/References/ExportFile.cs
@@ -0,0 +1,9 @@
+﻿namespace EuroHelp.Web.Models.References
+{
+    public class ExportFile
+    {
+        public string StartDate { get; set; }
+
+        public string EndDate { get; set; }
+    }
+}
