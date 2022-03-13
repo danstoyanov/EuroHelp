bec75d8652fa7eb306029b302e5ddc52fbe188ef
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-03-13 20:49:20 +0200

    🆕 Custom extensin methods !

---
+++ EuroHelp/EuroHelp.Web/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace EuroHelp.Web.Infrastructure
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetId(this ClaimsPrincipal user)
+            => user.FindFirst(ClaimTypes.NameIdentifier).Value;
+    }
+}
