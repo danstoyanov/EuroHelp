73e4ff7d13f984c59f09306c62f28517297bb258
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-03-31 20:45:10 +0300

    🛠 All damages view is fixed ! 🛠 Admin view panel is fixed ! 🛠 Damage paging is fixed !

---
+++ EuroHelp/EuroHelp.Data/Migrations/20220331110040_RemoveDamageImg.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EuroHelp.Data.Migrations
+{
+    public partial class RemoveDamageImg : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DamageImage",
+                table: "Damages");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DamageImage",
+                table: "Damages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
